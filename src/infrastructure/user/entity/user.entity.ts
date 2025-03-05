ba71f6ad3a3f32833096cc1c3bdc1423507ab2cb
dudgns2824 <dudgns2824@gmail.com>
2025-03-05 15:03:23 +0900

    read.me 변경

--- src/infrastructure/user/entity/user.entity.ts
+++ src/infrastructure/user/entity/user.entity.ts
@@ -1,5 +1,5 @@
 import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
-import { UserRole } from '../../../domain/user/enumeration/userrole';
+import { UserRole } from '../../../domain/user/enumeration/user-role';
 
 @Entity('users')
 export class UserEntity {
